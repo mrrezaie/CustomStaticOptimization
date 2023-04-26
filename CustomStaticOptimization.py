cb8fce724801bd925f70a4c892b3afa8e90a3546
mrrezaie <mrrezaie@outlook.com>
2023-04-27 01:44:51 +0330

    minor update
    - make tendons of muscles which OFL is greater than TSL rigid.
    - add force-velocity multiplier to active element

--- CustomStaticOptimization.py
+++ CustomStaticOptimization.py
@@ -6,7 +6,8 @@ from time import time
 import opensim as osim
 osim.Logger.setLevel(4)
 
-cycle = [0.83, 1.58] # stance time
+cycle = [0.83, 1.58] # stance time Rajagopal
+# cycle = [0.6, 1.4] # stance time Gait2392
 
 model = osim.Model('scaled.osim')
 state = model.initSystem()
@@ -15,23 +16,40 @@ nMuscles = model.getMuscles().getSize()
 
 MIF	= np.empty(nMuscles) # muscle maximum isometric force
 OFL	= np.empty(nMuscles) # optimal fiber length
-ML  = np.empty(nMuscles) # muscle length
+TSL	= np.empty(nMuscles) # tendon slack length
 # PA	= np.empty(nMuscles) # muscle pennation angle
-# TSL	= np.empty(nMuscles) # tendon slack length
+# ML  = np.empty(nMuscles) # muscle length
 
 for i,muscle in enumerate(model.updMuscles()):
 	# muscle = osim.Millard2012EquilibriumMuscle.safeDownCast(muscles.get(i))
-	muscle.set_ignore_activation_dynamics(False) # activation dynamics (have no impact)
-	muscle.set_ignore_tendon_compliance(False) # compliant tendon
+
 	MIF[i] = muscle.getMaxIsometricForce()
 	OFL[i] = muscle.getOptimalFiberLength()
-	ML[i]  = muscle.getLength(state)
-	# PA[i]     = muscle.getPennationAngle(state)
-	# TSL[i]    = muscle.getTendonSlackLength()
+	TSL[i] = muscle.getTendonSlackLength()
+	# PA[i]  = muscle.getPennationAngleAtOptimalFiberLength()
+	# ML[i]  = muscle.getLength(state)
+
+	muscle.set_ignore_activation_dynamics(False) # activation dynamics (have no impact)
+
+	if muscle.getTendonSlackLength() < muscle.getOptimalFiberLength():
+		muscle.set_ignore_tendon_compliance(True) # rigid tendon
+		print('rigid     tendon: ', muscle.getName())
+	else:
+		muscle.set_ignore_tendon_compliance(False) # compliant tendon
+		print('compliant tendon: ', muscle.getName())
 
 state = model.initSystem()
-'''Rajagopal et al. 2016. Tendons were modeled as rigid when the tendon slack length was less than the muscle optimal fiber length.'''
-'''when set_ignore_tendon_compliance is true, the fiber_length is omitted from state and causes a change in state size'''
+'''Rajagopal et al. 2016. Tendons were modeled as rigid when the 
+tendon slack length was less than the muscle optimal fiber length.
+
+In rigid tendons, there is no fiber_length in state.
+
+in Millard2012EquilibriumMuscle:
+ignoreTendonCompliance     computeEquilibrium     ForceVelocityMultiplier
+        False                     False                     0
+        False                     True                      1
+        True                      False                     values
+        True                      True                      values'''
 
 # get coordinates' value, speed and moment from IK and ID files
 q = osim.TimeSeriesTable('inverse_kinematics.mot')
@@ -45,7 +63,7 @@ GCVS = osim.GCVSplineSet(q)
 firstDerivative = osim.StdVectorInt(); firstDerivative.push_back(0)
 u = osim.TimeSeriesTable(osim.StdVectorDouble(t))
 for i in q.getColumnLabels():
-	speed = [GCVS.get(i).calcDerivative(firstDerivative,  osim.Vector(1,j)) for j in t]
+	speed = [GCVS.get(i).calcDerivative(firstDerivative, osim.Vector(1,j)) for j in t]
 	u.appendColumn(i, osim.Vector(speed))
 u.addTableMetaDataString('inDegrees', 'no')
 u.addTableMetaDataString('nColumns', str(u.getNumColumns()))
@@ -65,7 +83,6 @@ for i in q.getColumnLabels():
 	m.appendColumn(i, osim.Vector(values))
 	m.removeColumn(name)
 
-mMat = m.getMatrix()
 # osim.STOFileAdapter().write(q, 'q.sto')
 # osim.STOFileAdapter().write(u, 'u.sto')
 # osim.STOFileAdapter().write(m, 'm.sto')
@@ -77,20 +94,11 @@ for i,coordinate in enumerate(model.getCoordinateSet()):
 		if j in coordinate.getName():
 			ok[i] = False
 
+def objFun(a):  # sum of squared muscle activation
+	return np.sum(volume*length*(a)**2)
 
 '''Equality constraint means that the constraint function result is to be zero 
 whereas inequality means that it is to be non-negative.'''
-
-# PCSA = max isometric force / 60
-# muscle volume = muscle length * PCSA
-# muscle stress = muscle force / PCSA
-# contractile element = activity * max isometric force * active force-length multiplier * force-velocity multiplier
-# parallel elasic element = max isometric force * passive force-length multiplier
-# fiber force along tendon = (contractile element + parallel elasic element) * cos pennation angle
-
-def objFun(a):  # sum of squared muscle activation [volume weighted, power==3]
-	# return np.sum((a)**2)
-	return np.sum(volume*length*(a)**2) # volume*length*
 
 def eqConstraint(a):  # A.dot(x)-b  == np.sum(A*x,axis=1)-b
 	# return momentArm.dot(a*MIF) - moment # classic static optimization
@@ -101,16 +109,16 @@ def eqConstraint(a):  # A.dot(x)-b  == np.sum(A*x,axis=1)-b
 
 PCSA = MIF / 60 # Rajagopal et al. (2016) (N/cm^2)
 volume = PCSA * OFL
-length = ML
+length = OFL + TSL # ML
 
 constraints = ({'type':'eq', 'fun':eqConstraint})
 init = [0.1 for _ in range(nMuscles)] # initial guess of muscle activity (0.1)
-lb   = [0   for _ in range(nMuscles)] # lower bound (0)
-ub   = [1   for _ in range(nMuscles)] # max activity >= 1
+lb   = [0.0 for _ in range(nMuscles)] # lower bound (0)
+ub   = [1.0 for _ in range(nMuscles)] # max activity >= 1
 
 activity = osim.TimeSeriesTable()
 activity.addTableMetaDataString('inDegrees', 'no')
-activity.setColumnLabels(osim.StdVectorString([joint.getName() for joint in model.getMuscles()]))
+activity.setColumnLabels([joint.getName() for joint in model.getMuscles()])
 force = activity.clone()
 
 ts = time()
@@ -118,57 +126,61 @@ for i,ii in enumerate(t):
 	state.setTime(ii)
 
 	# update coordinates' values
-	for coordinate in model.updCoordinateSet():
-		name = coordinate.getName()
-		coordinate.setValue(state, q.getDependentColumn(name)[i], False)
-		coordinate.setSpeedValue(state, u.getDependentColumn(name)[i])
+	value = osim.RowVector(q.getRowAtIndex(i))
+	speed = osim.RowVector(u.getRowAtIndex(i))
+	for j,coordinate in enumerate(model.updCoordinateSet()):
+		coordinate.setValue(state, value[j], False)
+		coordinate.setSpeedValue(state, speed[j])
 		
 	model.assemble(state)
 	# model.realizePosition(state)
-	model.realizeVelocity(state)
-	# model.realizeDynamics(state)
+	# model.realizeVelocity(state)
+	model.realizeDynamics(state)
 	# model.realizeAcceleration(state)
 	# model.equilibrateMuscles(state)
 
 	# muscle parameters at each time frame
-	L   = np.empty(nMuscles) # muscle length
+	# L   = np.empty(nMuscles) # muscle length
 	CPA = np.empty(nMuscles) # cos Pennation angle
 	FLM	= np.empty(nMuscles) # active force length multiplier
-	FVM	= np.empty(nMuscles) # force velocity multiplier
 	PFM = np.empty(nMuscles) # passive force multiplier
+	FVM	= np.empty(nMuscles) # force velocity multiplier
 	MA 	= np.zeros((sum(ok), nMuscles)) # muscle moment arm (for specific coordinates only)
 
 	for j,muscle in enumerate(model.getMuscles()):
-		muscle = osim.Millard2012EquilibriumMuscle.safeDownCast(muscle)
+		# muscle = osim.Millard2012EquilibriumMuscle.safeDownCast(muscle)
 		muscle.setActivation(state, 1)
 		muscle.computeEquilibrium(state)
 
-		L[j]   = muscle.getLength(state)
+		# L[j]   = muscle.getLength(state)
 		CPA[j] = muscle.getCosPennationAngle(state)
 		FLM[j] = muscle.getActiveForceLengthMultiplier(state)
-		FVM[j] = muscle.getForceVelocityMultiplier(state)
 		PFM[j] = muscle.getPassiveForceMultiplier(state)
+		FVM[j] = muscle.getForceVelocityMultiplier(state)
 
 		indx = 0
 		for k,coordinate in enumerate(model.getCoordinateSet()):
 			if ok[k]: # increases the speed
-				MA[indx,j] = muscle.computeMomentArm(state, coordinate)
-				# if abs(temp) > 1e-6:
-				# 	MA[indx,j] = temp
+				temp = muscle.computeMomentArm(state, coordinate)
+				if abs(temp) > 1e-4: # ignore values less than 0.1 mm
+					MA[indx,j] = temp
 				indx += 1
 
 	# moment = m.getRowAtIndex(i).to_numpy()[ok] # THERE IS A BUG HERE
-	moment = np.array([mMat.getElt(i,j) for j in range(len(ok))])[ok] # 1D (nCoordinates)
+	moment = osim.RowVector(m.getRowAtIndex(i)).to_numpy()[ok] # 1D (nCoordinates)
 	momentArm = MA # 2D (nCoordinate, nMuscles)
-	activeElement  = MIF*FLM*CPA # along tendon 1D (nMuscles) [FVM in compliant tendon is 1 (automatically excluded)]
-	passiveElement = MIF*PFM*CPA # along tendon 1D (nMuscles)
+	activeElement  = MIF*FLM*FVM*CPA # along tendon, 1D (nMuscles)
+	passiveElement = MIF*PFM*CPA # along tendon, 1D (nMuscles)
 
-	out = minimize(objFun, x0=init, method='SLSQP', bounds=Bounds(lb,ub), constraints=constraints, options={'maxiter':500}, tol=1e-06)
+	out = minimize(objFun, x0=init, method='SLSQP', bounds=Bounds(lb,ub), \
+				   constraints=constraints, options={'maxiter':500}, tol=1e-06)
 	init = out['x']
 	activity.appendRow(ii, osim.RowVector(out['x']))
 	force.appendRow(ii, osim.RowVector(activeElement * out['x'] + passiveElement))
 
-	print(f"Optimization ... {(i+1):0>3d}/{len(t):0>3d} ({round(ii,3):.3f}) success={out['success']} fun={round(out['fun'],3)}")
+	print(f'Optimization ... {(i+1):0>3d}/{len(t):0>3d} ({round(ii,3):.3f})', \
+	 	  f"success={out['success']} fun={round(out['fun'],3)}")
+
 	if out['status'] != 0: 
 		print(f"\t\t\tmessage: {out['message']} ({out['status']})")
 
@@ -176,11 +188,11 @@ print(f'Optimization ... finished in {time()-ts:.2f} s')
 
 # write muscles activity and force to sto files
 activity.addTableMetaDataString('nColumns', str(activity.getNumColumns()))
-activity.addTableMetaDataString('nRows', str(activity.getNumRows()))
+activity.addTableMetaDataString('nRows',    str(activity.getNumRows()))
 force.addTableMetaDataString('nColumns', str(force.getNumColumns()))
-force.addTableMetaDataString('nRows', str(force.getNumRows()))
+force.addTableMetaDataString('nRows',    str(force.getNumRows()))
 osim.STOFileAdapter().write(activity, 'activity.sto')
-osim.STOFileAdapter().write(force, 'force.sto')
+osim.STOFileAdapter().write(force,    'force.sto')
 
 
 plt.figure()
@@ -198,15 +210,15 @@ plt.xlabel('Time (s)')
 plt.savefig('activity.png')
 plt.show(block=False)
 
-plt.figure()
-# plt.plot(t, force.getDependentColumn('soleus_r').to_numpy(), label='soleus_r')
-# plt.plot(t, force.getDependentColumn('gasmed_r').to_numpy(), label='gast med_r')
-# plt.plot(t, force.getDependentColumn('gaslat_r').to_numpy(), label='gast lat_r')
-# plt.plot(t, force.getDependentColumn('perlong_r').to_numpy(), label='per_long_r')
-plt.plot(t, force.getDependentColumn('soleus_l').to_numpy(), label='soleus_l')
-plt.plot(t, force.getDependentColumn('gasmed_l').to_numpy(), label='gast med_l')
-plt.plot(t, force.getDependentColumn('gaslat_l').to_numpy(), label='gast lat_l')
-plt.plot(t, force.getDependentColumn('perlong_l').to_numpy(), label='per_long_l')
-plt.legend()
-plt.title('Muscle Force')
-plt.show(block=False)
+# plt.figure()
+# # plt.plot(t, force.getDependentColumn('soleus_r').to_numpy(), label='soleus_r')
+# # plt.plot(t, force.getDependentColumn('gasmed_r').to_numpy(), label='gast med_r')
+# # plt.plot(t, force.getDependentColumn('gaslat_r').to_numpy(), label='gast lat_r')
+# # plt.plot(t, force.getDependentColumn('perlong_r').to_numpy(), label='per_long_r')
+# plt.plot(t, force.getDependentColumn('soleus_l').to_numpy(), label='soleus_l')
+# plt.plot(t, force.getDependentColumn('gasmed_l').to_numpy(), label='gast med_l')
+# plt.plot(t, force.getDependentColumn('gaslat_l').to_numpy(), label='gast lat_l')
+# plt.plot(t, force.getDependentColumn('perlong_l').to_numpy(), label='per_long_l')
+# plt.legend()
+# plt.title('Muscle Force')
+# plt.show(block=False)
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # cunstom Static Optimization 
 WIP
 
+Typical Static Optimization, i.e., minimize the sum of squared muscle activation
+![sample](./activity0.png)
+
+Static Optimization with weighted cost function
 ![sample](./activity.png)
